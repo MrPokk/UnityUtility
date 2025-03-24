007a22b96a34f71f2ea5f8a1003a630993d0815e
Kirill <96985733+MrPokk@users.noreply.github.com>
2025-03-24 11:33:43 +0300

    Add files via upload

--- ScriptBuildProfiles.cs
+++ ScriptBuildProfiles.cs
@@ -1,49 +1,86 @@
 #if UNITY_6000_0_OR_NEWER && UNITY_EDITOR
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEditor.Build.Profile;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 public class ScriptBuildProfiles : EditorWindow
 {
+
+    private const string _directoryBuild = "Assets/Settings/Build Profiles/";
+
     [MenuItem("File/Build All Platforms ^#&P", priority = 212)]
     private static void BuildPlatforms()
     {
+        t:
+        if (!Directory.Exists(_directoryBuild))
+            Directory.CreateDirectory(_directoryBuild);
 
-        if (!Directory.Exists("Assets/Settings/Build Profiles/"))
-            Directory.CreateDirectory("Assets/Settings/Build Profiles/");
-
-        string[] Files = Directory.GetFiles("Assets/Settings/Build Profiles/");
+        var Files = Directory.GetFiles(_directoryBuild);
 
         if (Files.Length == 0)
             throw new NullReferenceException("ERROR: No Build Profiles found");
 
-        foreach (string Element in Files)
+        foreach (var Element in FindAllBuildProfiles())
         {
-            if (Path.GetExtension(Element) != ".asset") continue;
-            BuildProfile BuildProfile = AssetDatabase.LoadAssetAtPath<BuildProfile>(Element);
-
-            if (BuildProfile == null)
+            if (Element == null)
                 continue;
 
-            SimpleBuild(BuildProfile, BuildProfile.name);
+            SimpleBuild(Element);
+            
             Wait(() => BuildPipeline.isBuildingPlayer);
         }
-        Debug.Log("Build All Platforms");
     }
 
-    private static void SimpleBuild(BuildProfile buildProfile, string Name)
+    private static void SimpleBuild(BuildProfile buildProfile)
     {
-        BuildPlayerWithProfileOptions options = new BuildPlayerWithProfileOptions();
 
-        options.buildProfile = buildProfile;
-        options.locationPathName = $"Build/{Name}/{Name}.exe";
-        options.options = BuildOptions.None;
+        BuildReport Report = BuildPipeline.BuildPlayer(GetBuildOptions(buildProfile));
 
-        BuildPipeline.BuildPlayer(options);
+        OnValidate(Report, buildProfile.name);
+    }
+
+    private static List<BuildProfile> FindAllBuildProfiles()
+    {
+        List<BuildProfile> profiles = new List<BuildProfile>();
+        string[] guids = AssetDatabase.FindAssets("t:BuildProfile");
+
+        foreach (string guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            BuildProfile profile = AssetDatabase.LoadAssetAtPath<BuildProfile>(path);
+
+            if (profile != null)
+                profiles.Add(profile);
+        }
+
+        return profiles;
+    }
+
+
+    private static BuildPlayerWithProfileOptions GetBuildOptions(BuildProfile buildProfile)
+    {
+        BuildPlayerWithProfileOptions PlayerOptions = new BuildPlayerWithProfileOptions();
+
+        var NameProfile = buildProfile.name;
+
+        PlayerOptions.buildProfile = buildProfile;
+        PlayerOptions.locationPathName = $"Build/{NameProfile}/{NameProfile}.exe";
+
+        return PlayerOptions;
+    }
+
+    private static void OnValidate(BuildReport report, string nameProfile)
+    {
+        if (report.summary.result == BuildResult.Succeeded)
+            Debug.Log($"<color=green>Build succeeded:</color> {nameProfile}");
+        else
+            Debug.LogError($"Build failed for profile: {nameProfile}");
     }
 
     private static IEnumerator Wait(Func<bool> op)
