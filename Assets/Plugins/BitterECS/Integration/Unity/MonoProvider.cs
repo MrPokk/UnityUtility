58827517d7b3343c19e240a83867dcde464b0773
Kirill <dr.mokrousoff2017@yandex.ru>
2025-11-29 16:57:11 +0300

    Fix bag

--- Assets/Plugins/BitterECS/Integration/Unity/MonoProvider.cs
+++ Assets/Plugins/BitterECS/Integration/Unity/MonoProvider.cs
@@ -66,7 +66,7 @@ namespace BitterECS.Integration
         }
     }
 
-    public class MonoProvider<T> : MonoProvider where T : EcsEntity
+    public class MonoProvider<T> : MonoProvider where T : EcsPresenter
     {
         protected override void Awake()
         {
